b4d4496a70168d8420c9130b25575cf912693c63
Robin Nyzell <nyzell_@hotmail.com>
2020-06-18 16:52:06 +0200

    Theme change

---
+++ wp-content/themes/blankslate/footer.php
@@ -0,0 +1,12 @@
+</div>
+<footer id="footer">
+<div id="copyright">
+&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+
+<p> Test paragraf, mäktigt!! </p>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
